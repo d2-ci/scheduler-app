bf8195c0d948d125ff99973968c3bce6b7f5793e
GitHub <noreply@github.com>
2021-02-22 09:25:27 +0000

    863ff5f chore(deps-dev): bump eslint-plugin-import from 2.20.1 to 2.22.1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6546c330044aa896e39940766a5afeac.js"
+  "./precache-manifest.caba1da183cd1ce79da33590f0cd376a.js"
 );
 
 self.addEventListener('message', (event) => {
